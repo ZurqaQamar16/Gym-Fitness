48ba9aa534709e42e260f76ce05f15ba253bded4
Zurqa Qamar <121165087+ZurqaQamar16@users.noreply.github.com>
2024-06-25 12:38:32 +0500

    Add files via upload

---
+++ Frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        Designed And Developed By Sabi Ul Hassan & Hammad Bin Nasir
+    </footer>
+  )
+}
+
+export default Footer
---
+++ Frontend/src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <header>
+      <p>GYM FITNESS</p>
+    </header>
+  )
+}
+
+export default NavBar
